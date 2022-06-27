558239501deb9ab862d9cbfd0dc044b9adb58e33
Theo1982 <etereoeternauta@gmail.com>
2022-06-27 15:29:33 -0300

    Add files via upload
    actualizacion

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+const boton = document.querySelector(".switch_background");
+boton.addEventListener("click", ()=>{
+    boton.classList.toggle("active");
+});
+
+function copy() {
+  alert("Autor: Octavio A. Macchi. EMAIL: octavio_macchi@hotmail.com");
+}
